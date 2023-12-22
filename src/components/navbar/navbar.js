4c89c5b407a7d7b28a89a335556cc718c0acd100
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-12-22 16:10:25 +0530

    created navbar with necesssary links

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <a href="#">SELFOCUS</a>
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#selfocus">What is Selfocus?</a>
+          </p>
+          <p>
+            <a href="#features">Features</a>
+          </p>
+          <p>
+            <a href="#library">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <button type="button">GET EXTENSION</button>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
