630bda19503459c347fedaaf8b8355500e8d4352
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-12-22 16:42:52 +0530

    created toggle menu logic for toggling between menu and regular navbar based on screen resize

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 
-const navbar = () => {
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -21,13 +23,51 @@ const navbar = () => {
           <p>
             <a href="#library">Library</a>
           </p>
+          <p>
+            <a href="https://sendfox.com/lp/3l7v4l" target="_blank">
+              News
+            </a>
+          </p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
         <button type="button">GET EXTENSION</button>
       </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine size={27} onClick={() => setToggleMenu(false)} />
+        ) : (
+          <RiMenu3Line size={27} onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#selfocus">What is Selfocus?</a>
+              </p>
+              <p>
+                <a href="#features">Features</a>
+              </p>
+              <p>
+                <a href="#library">Library</a>
+              </p>
+              <p>
+                <a href="https://sendfox.com/lp/3l7v4l" target="_blank">
+                  News
+                </a>
+              </p>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <button type="button">GET EXTENSION</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
