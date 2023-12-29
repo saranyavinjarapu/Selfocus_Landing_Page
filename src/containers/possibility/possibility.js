fe64f9be951b5751545b010e98b9a5aed3f0116b
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-12-29 14:01:22 +0530

    added logic to make feature component accommodate an image prop passed

--- src/containers/possibility/possibility.js
+++ src/containers/possibility/possibility.js
@@ -1,32 +1,49 @@
 import React from "react";
 import { Feature } from "../../components";
 import "./possibility.css";
-import YourSvg from "../../assets/ff.svg";
+import NewTab from "../../assets/new_tab.svg";
+import Design from "../../assets/design.svg";
+import Goals from "../../assets/goals.svg";
+import Session from "../../assets/session.svg";
+import Block from "../../assets/block.svg";
+import Background from "../../assets/background.svg";
 
 const possibilityData = [
   {
     title: "New-tab dashboard",
     text: "Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.",
+    imgPath: NewTab,
+    imgAlt: "New Tab Dashboard Icon",
   },
   {
     title: "Minimalistic design",
     text: "An all-in-one customer service platform that helps you balance everything your customers need to be happy.",
+    imgPath: Design,
+    imgAlt: "Minimalistic Design Icon",
   },
   {
     title: "Assign daily goals",
     text: "Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.",
+    imgPath: Goals,
+    imgAlt: "Daily Goals Icon",
   },
   {
     title: "Create work sessions",
     text: "Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion.",
+    imgPath: Session,
+    imgAlt: "Work Sessions Icon",
   },
   {
     title: "Block distracting websites",
     text: "Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools.",
+    imgPath: Block,
+    imgAlt: "Block Distracting Websites Icon",
   },
   {
     title: "Momentum inspired backgrounds",
     text: "We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help.",
+    imgPath: Background,
+    imgAlt: "Inspirational Backgrounds Icon",
   },
 ];
 
@@ -38,27 +55,18 @@ const possibility = () => {
     >
       <div className="selfocus__possibilities-possibility ">
         <Feature
-          title="Features"
+          title="Library"
           text="Launched in Chrome Extensions, Productivity, Task Management. Our inspiring nature backgrounds and Zen music keep you calm and focused. These methods are scientifically proven to be efficient in increasing your focus and productivity while reducing stress"
         />
       </div>
-
-      <img
-        style={{
-          width: "1.5rem",
-          height: "1.5rem",
-          fill: "#39afa3 !important",
-        }}
-        src={YourSvg}
-        alt="Your SVG"
-      />
 
       <div className="selfocus__possibilities-container">
         {possibilityData.map((item, index) => (
           <Feature
             title={item.title}
             text={item.text}
-            imagePath=""
+            imagePath={item.imgPath}
+            imageAlt={item.imgAlt}
             key={item.title + index}
           />
         ))}
